961b575666883d7572be91a6587898ff002a38f9
ibtissemayariii <ebtissemayari18@gmail.com>
2026-01-30 17:39:12 +0100

    Fix: use localstack service and container networking

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,8 +5,9 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true  
+  s3_use_path_style           = true
 
   endpoints {
-    s3 = "http://localhost:4566"
+    s3 = "http://localstack:4566"
   }
 }
